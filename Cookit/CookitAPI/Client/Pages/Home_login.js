7efcb8241521d4360fd87825d7afaa34a6cb0e33
אורלי דניאלוב <orlydanielov@outlook.com>
2019-07-16 20:52:15 +0300

    Revert "חסר מטלה ו ויש שגיאות"
    This reverts commit a6fbc7b4c19714f3e84ec8bded8e622456a6ab53.

---
+++ Cookit/CookitAPI/Client/Pages/Home_login.js
@@ -0,0 +1,7 @@
+﻿//Link_Hello_to_User
+// הפונקציה קוראת בתחילת הקריאה לדף
+$(document).ready(function () {
+    // הוספת הודעה : "שלום לקוח" + התנתקות
+    var user_name = JSON.parse(sessionStorage.getItem("Login_User")).first_name;    // שם המשתמש המחובר
+    $("#lbl_Hello_to_User").html("שלום "+user_name);
+});
