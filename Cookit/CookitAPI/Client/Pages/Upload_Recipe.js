2cd1c3557704a4ce6503c810c3b8f856d8ee0d6e
OrlyDanielov <33680764+OrlyDanielov@users.noreply.github.com>
2019-10-31 23:38:10 +0200

    הוספה דינמית של מצרך
    צריך לתקן את התצוגה של המצרך המוסף, ולהוסיף למאפיינים את המידע באופן דינאמי.

--- Cookit/CookitAPI/Client/Pages/Upload_Recipe.js
+++ Cookit/CookitAPI/Client/Pages/Upload_Recipe.js
@@ -1,29 +1,71 @@
-﻿// הפונקציה קוראת בתחילת הקריאה לדף
+﻿//*******************************************************************************************
+// globals variables
+//*******************************************************************************************
+//הקטגוריות
+var ARRY_DISH_TYPE = new Array();
+var ARRY_DISH_CATEGORY = new Array();
+var ARRY_FOOD_TYPE = new Array();
+var ARRY_KITCHEN_TYPE = new Array();
+var ARRY_DIFFICULTY_LEVEL = new Array();
+var ARRY_INGRIDIANTS = new Array();
+var ARRY_MESURMENTS = new Array();
+//בדיקת תקינות
+var RECIPE_VALIDATION = {
+    name: false ,
+    recp_dish_type: false,
+    recp_dish_category: false,
+    recp_food_type: false,
+    recp_kitchen_type: false,
+    recp_level: false,
+    recp_total_time: false,
+    recp_work_time: false,
+    recp_steps: false
+};
+//מונה מספר מצרכים
+var COUNT_INGRIDIANTS = 1;
+
+//*******************************************************************************************
+// page load
+//*******************************************************************************************
+
+// הפונקציה קוראת בתחילת הקריאה לדף
 $(document).ready(function () {
    //סוגי המנות ממסד הנתונים
-    var arry_dish_type = new Array();
     GetDishType();
     // קטגוריות מנות מהמסד נתונים
-    var arry_dish_category = new Array();
     GetDishCategory();
     //עדכון סוג אוכל
-    var arry_food_type = new Array();
     GetFoodType();
     //עדכון סגנון מטבח
-    var arry_kitchen_type = new Array();
     GetKitchenType();
     //עדכון דרגת קושי
-    var arry_difficulty_level = new Array();
     GetDifficultyLevel();
     // עדכון שם מצרך
-    var arry_ingridiants = new Array();
     GetIngridiants();
     //עדכון אופן מדידה של מצרך
-    var arry_Mesurments = new Array();
     GetMesurments();
 });
 //*******************************************************************************************
-//////////////////////////////////////////////////////
+// INIT DATA INTO DROP DOWN LIST
+//*******************************************************************************************
+//הפונקציה מכניסה את ערכי מהבסיס נתונים באופן דינמי אל רשימה נגללת
+function EnterData2DDList(arry, ddList) {      
+    for (i in arry) {
+        $("#" + ddList).append(AddOption(arry[i]));
+    }
+}
+
+//הפונקציה מוסיפה אופציה לרשימה הנגללת
+function AddOption(item) {
+    var values = Object.values(item);//Object.keys(item);
+    var x = values[0];
+    var y = values[1];
+    return '<option value="' + x + '">' + y + '</option>';
+}
+
+//*******************************************************************************************
+// GET DISH TYPE
+//*******************************************************************************************
 // פונקציה מביאה את כל סוגי המנות
 function GetDishType() {
     // קריאה לפונקצית ajax של Get מהשרת עבור נתוני dishType
@@ -31,28 +73,18 @@ function GetDishType() {
 }
 
 function SuccessDishType(arry_dish_type) {
+    ARRY_DISH_TYPE = arry_dish_type;//JSON.parse(arry_dish_type);
     sessionStorage.setItem("Dish_Type", JSON.stringify(arry_dish_type));
-    initDishType(arry_dish_type);
+    EnterData2DDList(arry_dish_type, "select_dish_type");
 }
 
 function FailDishType() {
     console.log("שגיאה במשיכת נתוני מאפייני מנה מהשרת.");
     alert('שגיאה במשיכת נתוני מאפייני מנה מהשרת.');
 }
-
-//הפונקציה מכניסה את ערכי מהבסיס נתונים באופן דינמי אל רשימה נגללת
-function initDishType(data) {
-    var str;
-    for (i in data) {
-        $("#select_dish_type").append(AddOption_DishType(data[i]));
-    }
-}
-
-//הפונקציה מוסיפה אופציה לרשימה הנגללת
-function AddOption_DishType(item) {
-    return '<option value="' + item.id + '">' + item.dish_type + '</option>';
-}
-//////////////////////////////////////////////////////
+//*******************************************************************************************
+//  GET DISH CATEGORY
+//*******************************************************************************************
 // פונקציה מביאה את כל מאפייני המנות
 function GetDishCategory() {
         // קריאה לפונקצית ajax של Get מהשרת עבור נתוני dishCategoty
@@ -61,10 +93,9 @@ function GetDishCategory() {
 
 //הפונקציה מתבצעת במקרה ששאילת מאייפיני מנה התבצעה בהצלחה בשרת
 function SuccessDishCategory(arry_dish_category) {
-    //הכנסת הנתונים לsession stirage
+    ARRY_DISH_CATEGORY = arry_dish_category;
     sessionStorage.setItem("Dish_category", JSON.stringify(arry_dish_category));
-    // הוספת הנתונים לרשימה הנגללת
-    initDishCategory(arry_dish_category); 
+    EnterData2DDList(arry_dish_category, "select_dish_category"); 
 }
 
 //הפונקציה מתבצעת במקרה ששאילת מאייפיני מנה התבצעה בכישלון בשרת
@@ -72,22 +103,9 @@ function FailDishCategory() {
     console.log("שגיאה במשיכת נתוני מאפייני מנה מהשרת.");
     alert('שגיאה במשיכת נתוני מאפייני מנה מהשרת.');
 }
-
-//הפונקציה מכניסה את ערכי מהבסיס נתונים באופן דינמי אל רשימה נגללת
-function initDishCategory(data) { 
-    var str;
-    for (i in data) {
-        //        $("#select_dish_category").append(AddOption(data[i]));
-
-        $("#select_dish_category").append(AddOption_DishCategory(data[i]));
-    }
-}
-
-//הפונקציה מוסיפה אופציה לרשימה הנגללת
-function AddOption_DishCategory(item) {
-    return '<option value="' + item.id + '">' + item.dish_category + '</option>';   
-}
-//////////////////////////////////////////////////////
+//*******************************************************************************************
+//GET FOOD TYPE
+//*******************************************************************************************
 // פונקציה מביאה את כל סוגי האוכל
 function GetFoodType() {
     // קריאה לפונקצית ajax של Get מהשרת עבור נתוני foodType
@@ -95,28 +113,18 @@ function GetFoodType() {
 }
 
 function SuccessFoodType(arry_food_type) {
+    ARRY_FOOD_TYPE = arry_food_type;
     sessionStorage.setItem("Food_Type", JSON.stringify(arry_food_type));
-    initFoodType(arry_food_type);
+    EnterData2DDList(arry_food_type, "select_food_type");
 }
 
 function FailFoodType() {
     console.log("שגיאה במשיכת נתוני סוגי אוכל מהשרת.");
     alert('שגיאה במשיכת נתוני סוגי אוכל מהשרת.');
 }
-
-//הפונקציה מכניסה את ערכי מהבסיס נתונים באופן דינמי אל רשימה נגללת
-function initFoodType(data) {
-    var str;
-    for (i in data) {
-        $("#select_food_type").append(AddOption_FoodType(data[i]));
-    }
-}
-
-//הפונקציה מוסיפה אופציה לרשימה הנגללת
-function AddOption_FoodType(item) {
-    return '<option value="' + item.id + '">' + item.food_type + '</option>';
-}
-//////////////////////////////////////////////////////
+//*******************************************************************************************
+// GET KITCHEN TYPE
+//*******************************************************************************************
 // פונקציה מביאה את כל סוגי המטבחים
 function GetKitchenType() {
     // קריאה לפונקצית ajax של Get מהשרת עבור נתוני kitchenType
@@ -124,57 +132,36 @@ function GetKitchenType() {
 }
 
 function SuccessKitchenType(arry_kitchen_type) {
+    ARRY_KITCHEN_TYPE = arry_kitchen_type;
     sessionStorage.setItem("Kitchen_Type", JSON.stringify(arry_kitchen_type));
-    initKitchenType(arry_kitchen_type);
+    EnterData2DDList(arry_kitchen_type,"select_kitchen_type");
 }
 
 function FailKitchenType() {
     console.log("שגיאה במשיכת נתוני סוגי אוכל מהשרת.");
     alert('שגיאה במשיכת נתוני סוגי אוכל מהשרת.');
 }
-
-//הפונקציה מכניסה את ערכי מהבסיס נתונים באופן דינמי אל רשימה נגללת
-function initKitchenType(data) {
-    var str;
-    for (i in data) {
-        $("#select_kitchen_type").append(AddOption_KitchenType(data[i]));
-    }
-}
-
-//הפונקציה מוסיפה אופציה לרשימה הנגללת
-function AddOption_KitchenType(item) {
-    return '<option value="' + item.id + '">' + item.kitchen_type + '</option>';
-}
-//////////////////////////////////////////////////////
+//*******************************************************************************************
+// GET DIFFICULTY LEVEL
+//*******************************************************************************************
 // פונקציה מביאה את כל דרגות הקושי של מתכון
 function GetDifficultyLevel() {
-    // קריאה לפונקצית ajax של Get מהשרת עבור נתוני DifficultyLevel
     GlobalAjax("/api/DifficultyLevel", "GET", "", SuccessDifficultyLevel,FailDifficultyLevel);
 }
 
 function SuccessDifficultyLevel(arry_difficulty_level) {
+    ARRY_DIFFICULTY_LEVEL = arry_difficulty_level;
     sessionStorage.setItem("Difficulty_Level", JSON.stringify(arry_difficulty_level));
-    initDifficultyLevel(arry_difficulty_level);
+    EnterData2DDList(arry_difficulty_level,"select_kitchen_type");
 }
 
 function FailDifficultyLevel() {
     console.log("שגיאה במשיכת נתוני סוגי אוכל מהשרת.");
     alert('שגיאה במשיכת נתוני סוגי אוכל מהשרת.');
 }
-
-//הפונקציה מכניסה את ערכי מהבסיס נתונים באופן דינמי אל רשימה נגללת
-function initDifficultyLevel(data) {
-    var str;
-    for (i in data) {
-        $("#select_work_time").append(AddOption_DifficultyLevel(data[i]));
-    }
-}
-
-//הפונקציה מוסיפה אופציה לרשימה הנגללת
-function AddOption_DifficultyLevel(item) {
-    return '<option value="' + item.id + '">' + item.difficulty_level + '</option>';
-}
-//////////////////////////////////////////////////////
+//*******************************************************************************************
+// GET INGRIDIANTS
+//*******************************************************************************************
 // פונקציה מביאה את כל המצרכים של מתכון
 function GetIngridiants() {
     // קריאה לפונקצית ajax של Get מהשרת עבור נתוני Ingridiants
@@ -182,28 +169,19 @@ function GetIngridiants() {
 }
 
 function SuccessIngridiants(arry_ingridiants) {
+    ARRY_INGRIDIANTS = arry_ingridiants;
     sessionStorage.setItem("Ingridiants", JSON.stringify(arry_ingridiants));
-    initIngridiants(arry_ingridiants);
+    EnterData2DDList(arry_ingridiants,"select_ingridiant_name");
 }
 
 function FailIngridiants() {
     console.log("שגיאה במשיכת נתוני מצרכים מהשרת.");
     alert('שגיאה במשיכת נתוני מצרכים מהשרת.');
 }
+//*******************************************************************************************
+//  GET MESURMENTS
+//*******************************************************************************************
 
-//הפונקציה מכניסה את ערכי מהבסיס נתונים באופן דינמי אל רשימה נגללת
-function initIngridiants(data) {
-    var str;
-    for (i in data) {
-        $("#select_ingridiant_name").append(AddOption_Ingridiants(data[i]));
-    }
-}
-
-//הפונקציה מוסיפה אופציה לרשימה הנגללת
-function AddOption_Ingridiants(item) {
-    return '<option value="' + item.id + '">' + item.ingridinat + '</option>';
-}
-//////////////////////////////////////////////////////
 // פונקציה מביאה את כל אופני המדידה של מתכון
 function GetMesurments() {
     // קריאה לפונקצית ajax של Get מהשרת עבור נתוני Mesurments
@@ -211,93 +189,133 @@ function GetMesurments() {
 }
 
 function SuccessMesurments(arry_Mesurments) {
+    ARRY_MESURMENTS = arry_Mesurments;
     sessionStorage.setItem("Mesurments", JSON.stringify(arry_Mesurments));
-    initMesurments(arry_Mesurments);
+    EnterData2DDList(arry_Mesurments,"select_mesurment");
 }
 
 function FailMesurments() {
     console.log("שגיאה במשיכת נתוני אופן המדידה מהשרת.");
     alert('שגיאה במשיכת נתוני אופן המדידה מהשרת.');
 }
-
-//הפונקציה מכניסה את ערכי מהבסיס נתונים באופן דינמי אל רשימה נגללת
-function initMesurments(data) {
-    var str;
-    for (i in data) {
-        $("#select_mesurment").append(AddOption_Mesurments(data[i]));
-    }
-}
-
-//הפונקציה מוסיפה אופציה לרשימה הנגללת
-function AddOption_Mesurments(item) {
-    return '<option value="' + item.id + '">' + item.mesurment + '</option>';
-}
-
 //*******************************************************************************************
+// ADD INGRIDIANT
+//*******************************************************************************************
+function AddIngridiant()
+//מוסיף עוד שורה במתכון עבור מצרך נוסף
+{
+    COUNT_INGRIDIANTS = COUNT_INGRIDIANTS + 1;
 
-//////////////////////////////////////////////////////
-//// פונקציה שבודקת האם נבחר ערך לא ריק SELECT 
-//function IsNull() {
-//    /*
-//    //select_dish_type
-//    var selected_input = $('#select_dish_type').val();
-//    if (selected_input == null)
-//        alert("בבקשה בחר  'סוג מנה'.");
-//    //txt_dish_category
-//    var selected_input = $('#txt_dish_category').val();
-//    if (selected_input == null)
-//        alert("בבקשה בחר 'מאפיין מנה'.");
-//        */
-//    var all_select = $("input[type = select]");
-//    for (var i in all_select) {
-//        if (i.val() = null) {
-//            alert("בבקשה בחר 'מאפיין מנה'.");
-//        }
-//    }
-//}
+    var new_ingridiant = document.createElement('div');
+    new_ingridiant.id = "ingridiant_" + COUNT_INGRIDIANTS;
+    new_ingridiant.class="form-row";
+    //new_ingridiant.innerHTML = "היי!!";
+    //שם מצרך
+    var name_div = document.createElement("div");
+    name_div.class = "col";
 
-//////////////////////////////////////////////////////
-// פונקציה של העלאת מתכון
-function UpLoadRecipe() {
-    //אובייקט של מתכון חדש 
-    var newRecipe = {
-        Id_Recipe: parseInt(111),
-        Id_Recipe_User: parseInt(111),// לקחת מהseesion storage  ,
-        Name_Recipe: $('#txt_name_recipe').val(),
-        Id_Recipe_DishType: parseInt($('#select_dish_type').val()),
-        Id_Recipe_DishCategory: parseInt($('#txt_dish_category').val()),
-        Id_Recipe_FoodType: parseInt($('#select_food_type').val()),
-        Id_Recipe_KitchenType: parseInt($('#select_kitchen_type').val()),
-        RecipeTotalTime: Date.now(),
-        RecipeWorkTime: Date.now(),//($('#txt_work_time').val()),
-        Id_Recipe_Level: parseInt($('#txt_total_time').val()),//($('#select_work_time').val()),
-        PreparationSteps: $('#txt_preparation_steps').val()
+    var name_lbl = document.createElement("label");
+    name_lbl.for = "select_ingridiant_name_" + COUNT_INGRIDIANTS;
+    name_lbl.class = "col-sm-4 col-form-label";
+    name_lbl.innerHTML = "שם מצרך";
+
+    var name_select = document.createElement("select");
+    name_select.is = "select_ingridiant_name_" + COUNT_INGRIDIANTS;
+    name_select.class = "form-control";
+
+    var name_opt = document.createElement("option");
+    name_opt.value = "";
+    //name_opt.selected = true;
+    //name_opt.disabled = true;
+    name_opt.innerHTML = "שם מצרך";
+
+    name_select.appendChild(name_opt);
+
+    name_div.appendChild(name_lbl);
+    name_div.appendChild(name_select);
+    new_ingridiant.appendChild(name_div);
+    //כמות מצרך
+    var count_div = document.createElement("div");
+    count_div.class = "col";
+
+    var count_lbl = document.createElement("label");
+    count_lbl.for = "txt_ingridiant_amount_" + COUNT_INGRIDIANTS;
+    count_lbl.class = "col-sm-4 col-form-label";
+    count_lbl.innerHTML = "שם מצרך";
+
+    var count_input = document.createElement("input");
+    count_input.type = "text";
+    count_input.is = "txt_ingridiant_amount_" + COUNT_INGRIDIANTS;
+    count_input.class = "form-control text2rigth";
+    
+    count_div.appendChild(count_lbl);
+    count_div.appendChild(count_input);
+    new_ingridiant.appendChild(count_div);
+    //אופן מדידה
+    var mesurment_div = document.createElement("div");
+    mesurment_div.class = "col";
+
+    var mesurment_lbl = document.createElement("label");
+    mesurment_lbl.for = "select_mesurment_" + COUNT_INGRIDIANTS;
+    mesurment_lbl.class = "col-sm-4 col-form-label";
+    mesurment_lbl.innerHTML = "שם מצרך";
+
+    var mesurment_select = document.createElement("select");
+    mesurment_select.is = "select_mesurment_" + COUNT_INGRIDIANTS;
+    mesurment_select.class = "form-control";
+
+    var mesurment_opt = document.createElement("option");
+    mesurment_opt.value = "";
+    //mesurment_opt.selected = true;
+    //mesurment_opt.disabled = true;
+    mesurment_opt.innerHTML ="אופן מדידה";
+
+    mesurment_select.appendChild(mesurment_opt);
+
+    mesurment_div.appendChild(mesurment_lbl);
+    mesurment_div.appendChild(mesurment_select);
+    new_ingridiant.appendChild(mesurment_div);
+
+    var hr = document.createElement("hr");
+    hr.class = "mb - 4";
+    new_ingridiant.appendChild(hr);
+    //document.getElementById("recipe_ingridiants").appendChild(new_ingridiant);
+    document.getElementById("recipe_ingridiants").insertBefore(new_ingridiant, document.getElementById("btn_add_ingridiant"));
+    console.log("ingridiant " + COUNT_INGRIDIANTS);
+}
+//*******************************************************************************************
+// CHECK FORM VALIDATION
+//*******************************************************************************************
+function CheckFormValidation() {
+    //
+    Change_style_by_validation();
+}
+
+function Change_style_by_validation()
+//הפונקציה בודקת איזה פריט לא תקין ומסמנת אותו
+{
+    var recipe_inputs =
+    {
+        name: $("#user_first_name"),
+        recp_dish_type: $("#select_dish_type"),
+        recp_dish_category: $("#select_dish_category"),        
+        recp_food_type: $("#select_food_type"),
+        recp_kitchen_type: $("#select_kitchen_type"),
+        recp_level: $("#select_difficulty_level"),
+        recp_total_time: $("#txt_total_time"),
+        recp_work_time: $("#select_work_time"),
+        recp_steps: $("#txt_preparation_steps")
     };
-    //רשימה של המצרכים למתכון
-
-    //שמירת האובייקטים בsseion storage
-    sessionStorage.setItem("new_recipe", JSON.stringify(newRecipe));
-    console.log("לפני שליחה לשרת");
-    //שיגור הנתונים לשרת ע"י קריאה לפונקציה הAJAX
-    GlobalAjax("/api/Recipe", "POST", newRecipe, SucessFunc, FailFunc);
-    console.log("אחרי שליחה לשרת");
-
+    for (var i in user_validation) {
+        if (recipe_validation[i] == false)
+            recipe_inputs[i].addClass(" not_valid");
+        else {
+            if (recipe_inputs[i].hasClass("not_valid"))
+                recipe_inputs[i].removeClass("not_valid");
+        }
+    }
+    //צריך לעבור על כך המצרכים במתכון
 }
-
-//פונקציה שמתבצעת כאשר הנתונים נשלחו לשרת בהצלחה
-function SucessFunc() {
-    //window.location.href = "../ThankYou/ThankYou.html";
-    alert("המתכון הועלה בהצלחה לאתר!.");
-}
-
-//פונקציה שמתבצעת כאשר יש בעיה בשליחת הנתונים לשרת
-function FailFunc() {
-    /*
-    sessionStorage.removeItem("purchase");
-    sessionStorage.setItem("error", "Error occurred please try again");
-    window.location.href = "../ThirdParty/FailurePage.html";
-    */
-    sessionStorage.removeItem("newRecipe");
-    sessionStorage.setItem("error", "Error occurred please try again");
-    alert("תקלה, המתכון לא הועלה לאתר, נסה שוב מאוחר יותר!.");
-}
+//*******************************************************************************************
+// ADD NEW RECIPE
+//*******************************************************************************************
