228990cd75f31418169fb2b4a9255c9c21694e2e
OrlyDanielov <33680764+OrlyDanielov@users.noreply.github.com>
2019-11-20 14:47:40 +0200

    ולידציה של דף התחברות
    בודק שהקלטים לא ריקים ותיקנים לפני בדיקת בתחברות בשרת

--- Cookit/CookitAPI/Client/Pages/Login.js
+++ Cookit/CookitAPI/Client/Pages/Login.js
@@ -1,22 +1,22 @@
-﻿var EXPLANATION_PASSWORD = "הסיסמה באורך 6 עד 12 תווים, מכילה לפחות מספר אחד, ואות באנגלית.";
-var EXPLANATION_EMAIL = "אימייל צריך להיות באנגלית ועם הסימנים @ו .";
-//*******************************************************************************************
+﻿//*******************************************************************************************
 // globals variables
 //*******************************************************************************************
 
 
 var user = new Array();
 //פונקצית התחברות
+//*******************************************************************************************
+// LOGIN
+//*******************************************************************************************
 function LoginVerification() {
       // פרטי המשתמש הנכנס
     var email = $("#inputEmail").val();
     var pass = $("#inputPassword").val();  
     GlobalAjax("/api/User/" + email + '/' + pass, "GET", "", SuccessLogin, FailLogin);
-
 }
-
+function SuccessLogin(user)
 // פןנקציה של הצלחת זיהוי המשתמש
-function SuccessLogin(user) {
+{
     //console.log("login success!.");
     //alert("login success!.");
     // שמירת המשתמש המחובר והעברה לדף הבית
@@ -26,11 +26,11 @@ function SuccessLogin(user) {
         GlobalAjax("/api/Profile/GetProfileByUserId/" + user.id, "GET", "", SuccessProfile, FailProfile);
     else   //מעבר לדף הבית המחובר
         window.location.replace("Home_login.html");
-
 }
 
+function FailLogin()
 //פונקציה של אי הצלחת זיהוי המשתמש
-function FailLogin() {
+{
     console.log("login fail!.");
     alert("אימייל או סיסמה לא נכונים. אנא נסה שנית.");
 }
@@ -44,10 +44,75 @@ function FailProfile() {
     console.log("cant find match profile!.");
 
 }
+//*******************************************************************************************
+// INPUT VALIDATION
+//*******************************************************************************************
+function CheckInputs()
+//בודק האם הקלטים לא רייקים וחוקיים
+{
+    var password = $("#inputPassword").val();
+    var password_input = document.getElementById("inputPassword");
+    var password_feedback = document.getElementById("feedback_password");
+    var email = $("#inputEmail").val();
+    var email_input = document.getElementById("inputEmail");
+    var email_feedback = document.getElementById("feedback_email");
+    var flag = true;
+    // email
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email == "") {
+        email_input.classList.add("not_valid");
+        email_feedback.innerHTML = "אנא הכנס אימייל!";
+        flag = false;
+    }
+    /*else if (!(email.length <= 50)) {
+        email_input.classList.add("not_valid");
+        email_feedback.innerHTML = "אנא הכנס אמייל באורך עד 50 תווים!";
+        flag = false;
+    }*/
+    else if (re.test(String(email).toLowerCase()) == false) {
+        email_input.classList.add("not_valid");
+        email_feedback.innerHTML = "אנא הכנס אימייל תקין!.";
+        flag = false;
+    }
+    else {
+        if (email_input.classList.contains("not_valid"))
+            email_input.classList.remove("not_valid");
+        email_feedback.innerHTML = "";
+    }
+    // password
+    var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+    if (password == "") {
+        password_input.classList.add("not_valid");
+        password_feedback.innerHTML = "אנא הכנס סיסמה!";
+    }
+    /*else  if (!(password.match(passw))) {
+        password_input.classList.add("not_valid");
+        password_feedback.innerHTML = "אנא הכנס סיסמה באורך 6 עד 12 תווים הכוללת לפחות ספרה אחת, אות קטנה באנגלית ואות גדולה באנגלית!.";
+        flag = false;
+  }*/
+    else {
+        if (password_input.classList.contains("not_valid"))
+            password_input.classList.remove("not_valid");
+        password_feedback.innerHTML = "";
+    }
+    return flag;
+}
 
+//*******************************************************************************************
+// CHECK IF FORM VALID
+//*******************************************************************************************
+function CheckIfFormValid() {
+    if (CheckInputs())
+        LoginVerification();
+    else
+        alert("אנא תקן את המקומות המסומנים.");
+}
 //*******************************************************************************************
 // SHOW HIDE EXPLANATION
 //*******************************************************************************************
+//var EXPLANATION_PASSWORD = "הסיסמה באורך 6 עד 12 תווים, מכילה לפחות מספר אחד, ואות באנגלית.";
+//var EXPLANATION_EMAIL = "אימייל צריך להיות באנגלית ועם הסימנים @ו .";
+
 /*function ShowHideExplanation(btn_cliked_id) {
     var div_name = "explanation_";
     var words = btn_cliked_id.split('_');
